9d5d8df433502531197b8d8a77289f3de3c4d743
sorayahaidars <sorayahaidars14@gmail.com>
2024-03-28 11:07:17 +0700

    menambahkan dua class SayaTubeVideo dan SayaTubeUser

---
+++ modul6_1302223054/SayaTubeUser.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul6_1302223054
+{
+    internal class SayaTubeUser
+    {
+        public class sayaTubeUser
+        {
+            private int id;
+            public string username;
+            private List<SayaTubeVideo> uploadedVideos; 
+
+            public sayaTubeUser(string id)
+            {
+                this.username = id;
+                this.uploadedVideos = new List<SayaTubeVideo>();
+            }
+
+            public void GetTotalVideoPlayCount()
+            {
+                int play = 0;
+                foreach(SayaTubeVideo video in uploadedVideos)
+                {
+                    play = play + video.getplayCount();
+                }
+            }
+
+
+            public void AddVideo<SayaTubeVideo>()
+            {
+                sayaTubeUser sayaTubeUser = new sayaTubeUser(username);
+            }
+
+            public void PrintAllVideoPlaycount()
+            {
+                Console.WriteLine("\t User : " + username);
+            }
+        }
+    }
+}
---
+++ modul6_1302223054/SayaTubeVideo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul6_1302223054
+{
+    public class SayaTubeVideo
+    {
+        private int id { get => id; set => id = value; }
+        private string title { get => title; set => title = value; }
+        private int playCount { get => playCount; set => playCount = value; }
+
+        public SayaTubeVideo(string title)
+        {
+            this.title = title;
+            Random random = new Random();
+            this.id = random.Next();
+            this.playCount = 0;
+        }
+
+        public void setId(int id){this.id = id;}
+
+        public int getId() { return this.id;}
+
+        public void setTitle(string title) { this.title = title; }
+
+        public string getTitle() { return this.title; }
+
+        public void setplayCount(int playCount) { this.playCount = playCount; }
+
+        public int getplayCount() { return this.playCount; }
+
+        public void IncreasePlayCount(int playCount)
+        {
+            this.playCount++;
+        }
+
+        public void PrintVideoDetails(int id, string title, int playCount)
+        {
+            Console.WriteLine("\t Id : " + id);
+            Console.WriteLine("\t title : " + title);
+            Console.WriteLine("\t playCount : " + playCount);
+        }
+    }
+}
